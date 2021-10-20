b2e8b4a15152050d9ba38592f5300cd639812b84
VapoR <74574116+vaporrrr@users.noreply.github.com>
2021-10-20 11:18:30 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, VapoR'
 author = 'VapoR'
 
 # The full version, including alpha/beta/rc tags
-release = '2050'
+release = '2021'
 
 # -- General configuration ---------------------------------------------------
 
